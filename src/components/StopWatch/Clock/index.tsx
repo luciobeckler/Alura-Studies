a7ed866304e197984a7578a471eeec7713d723fe
Lucio_Beckler <luciobecklerp@gmail.com>
2023-08-06 17:21:08 -0300

    Exibindo tempo da tarefa no relógio

--- src/components/StopWatch/Clock/index.tsx
+++ src/components/StopWatch/Clock/index.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import style from "./Clock.module.scss";
 
-export default function Clock() {
+interface ClockProps {
+  tempo: number | undefined;
+}
+
+export default function Clock({ tempo = 0 }: ClockProps) {
+  const minutos = Math.floor(tempo / 60);
+  const segundos = tempo % 60;
+
+  const [minutosDezena = 0, minutosUnidade = 0] = String(minutos).padStart(
+    2,
+    "0"
+  );
+
+  const [segundosDezena = 0, segundosUnidade = 0] = String(segundos).padStart(
+    2,
+    "0"
+  );
+
   return (
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minutosDezena}</span>
+      <span className={style.relogioNumero}>{minutosUnidade}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{segundosDezena}</span>
+      <span className={style.relogioNumero}>{segundosUnidade}</span>
     </>
   );
 }
--- src/components/StopWatch/index.tsx
+++ src/components/StopWatch/index.tsx
@@ -3,7 +3,7 @@ import Clock from "./Clock";
 import style from "./StopWatch.module.scss";
 import { timeToSeconds } from "../../common/utils/dateTime";
 import { ITarefa } from "../../types/tarefa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface StopWatchProps {
   selecionado: ITarefa | undefined;
@@ -11,16 +11,18 @@ interface StopWatchProps {
 
 export default function StopWatch({ selecionado }: StopWatchProps) {
   const [tempo, setTempo] = useState<number>();
-  if (selecionado?.tempo) {
-    setTempo(timeToSeconds(selecionado.tempo));
-  }
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(timeToSeconds(selecionado.tempo));
+    }
+  }, [selecionado]);
 
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-      Tempo: {tempo}
       <div className={style.relogioWrapper}>
-        <Clock />
+        <Clock tempo={tempo} />
       </div>
       <Button type="button" texto="Iniciar " />
     </div>
